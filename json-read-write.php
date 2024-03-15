74c762e68a30a84a80cd64439caa6f3bb88fe25d
Micho983 <160588906+Micho983@users.noreply.github.com>
2024-03-15 12:56:36 +0100

    Add files via upload

---
+++ json-read-write.php
@@ -0,0 +1,48 @@
+<?php
+// read the contents of the file "polaznici.json"
+    print_r (__DIR__);
+    $students = file_get_contents( __DIR__."/polaznici.json") or die("Unable to open file 'polaznici.json'");
+    print_r ($students);
+
+    //decode the contents of the file "polaznici.json"
+    $students_data = json_decode($students, true);
+    print_r($students_data);
+
+    //add a new student to the array
+    $students_data[] = [
+        "name" => "Pero",
+        "surname" => "Perić",
+        "age"=> 25,
+        "phone" => "098/123-456",
+        "email" => "pero@pero.com",
+    ];
+
+    //encode the array of students
+    $students = json_encode($students_data);
+
+    //write the contents of the file "polaznici.json"
+    file_put_contents( __DIR__."/polaznici.json", $students) or die("Unable to write to file!");
+?>
+<!--Display data from the JSON file-->
+<table border="1" cellpadding="10">
+    <tr>
+        
+        <th>Ime</th>
+        <th>Prezime</th>
+        <th>Godine</th>
+        <th>Broj telefona</th>
+        <th>Email</th>
+        
+    </tr>
+    
+        <?php foreach ($students_data as $student): ?>
+        <tr>
+            <td><?php echo $student["name"]?></td>
+            <td><?php echo $student["surname"]?></td>
+            <td><?php echo $student["age"]?></td>
+            <td><?php echo $student["phone"]?></td>
+            <td><?php echo $student["email"]?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tr>    
+</table>
